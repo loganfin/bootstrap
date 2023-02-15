0ee883f2349949e5c92942bbcd6fec7bf6f2beb8
Logan Finley <finley4.logan@gmail.com>
2023-02-14 21:26:01 -0800

    Finished everything I think

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("click", function(e) {
-    if(e.target.classList.containts("m1")) {
+    if(e.target.classList.contains("w-100")) {
         const src = e.target.getAttribute("src");
         document.querySelector(".modal-img").src = src;
         const myModal = new bootstrap.Modal(document.getElementById('m1'))
